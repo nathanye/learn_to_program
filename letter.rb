76ce200a139d833531b78e1346ce7602a5430af1
nathanye <khalil54781@gmail.com>
2017-08-02 17:56:03 +0800

    backslash

--- letter.rb
+++ letter.rb
@@ -3,3 +3,24 @@
 puts 'Hello,world!'
 puts ''
 puts 'Good-bye.'
+
+# 字符串运算
+
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
